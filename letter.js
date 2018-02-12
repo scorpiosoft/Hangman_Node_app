67964dacbd2e2b5731ed33fae003a1f79169b846
Christopher Biessener <cpbiessener@gmail.com>
2018-02-11 18:16:25 -0600

    update comments

--- letter.js
+++ letter.js
@@ -1,11 +1,14 @@
 // The Letter class
 class Letter
 {
+  // constructor method
   constructor(letter)
   {
     this.the_letter = letter;
     this.guessed = false;
   }
+  // display method
+  // return the letter if guessed, else underscore
   display()
   {
     console.log(this.guessed);
@@ -14,14 +17,18 @@ class Letter
     else
       return '_';
   }
+  // guess method
+  // check argument vs the letter, set guessed true if a match
   guess(letter)
   {
     if (letter === this.the_letter)
       this.guessed = true;
     return this.guessed;
   }
+  // setter method for the_letter
   set the_letter(v)
   {
+    // validate the input
     if (v.length > 1)
     {
       console.log('Letters are a single character, yo!  You gave me [', v, ']');
@@ -30,6 +37,7 @@ class Letter
       this._the_letter = v;
     }
   }
+  // getter method for the_letter
   get the_letter()
   {
     return this._the_letter;
